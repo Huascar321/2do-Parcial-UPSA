a00123a27878ece2ab21163996836e98349a56da
visual_community@outlook.com <visual_community@outlook.com>
2019-11-30 10:04:53 -0400

    2do Parcial - Programacion Aplicada

---
+++ Biblioteca/Huascar/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Huascar")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Huascar")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("59342ed9-59ff-4348-a4d0-8b9533609194")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Biblioteca/Huascar/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Biblioteca;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Huascar
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Organizar
+            Person Cliente = new Person("Nelson Mandela", 1000);
+            double valoresperado = 1200;
+
+            //Actuar
+            Cliente.Save(200);
+            double valorobtenido = Cliente.SaveMoney;
+
+            //Afirmar
+            Assert.AreEqual(valorobtenido, valoresperado);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            //Organizar
+            Person Cliente = new Person("Nicolas Inmaduro", 1000);
+            double valoresperado = 900;
+
+            //Actuar
+            Cliente.Spend(100);
+            double valorobtenido = Cliente.SaveMoney;
+
+            //Afirmar
+            Assert.AreEqual(valorobtenido, valoresperado);
+        }
+    }
+}
